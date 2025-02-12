beb93d1f9cc4de7881b1af6cc26e2e70008019cd
user <user@gmail.com>
2025-02-12 10:40:03 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isEqual(StringBuilder s1, StringBuilder s2){
+        if(s1.compareTo(s2)==0){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Hello World");
+        StringBuilder sb1 = new StringBuilder("Hello");
+        StringBuilder sb2 = new StringBuilder("Hello");
+
+        if( isEqual(sb1, sb2)){
+            System.out.println("They are equal");
+        }
+
+
+        sb.insert(11, "!");
+
+        sb.reverse();
+        String str = sb.toString();
+
+        for(int i=0; i<sb.length(); i++){
+            char c = sb.charAt(i);
+            c = Character.toUpperCase(c);
+
+            sb.setCharAt(i, c);
+        }
+
+        System.out.println(sb);
+
+    }
+
+}
---
+++ src/Second.java
@@ -0,0 +1,26 @@
+public class Second {
+
+    public static int sum(int a, int b){
+        int result=a+b;
+        return result;
+    }
+    // Methods -  function
+    public static boolean isEven(int n){
+        if(n%2==0)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int n= sum(3,4);
+        System.out.println(n);
+
+        if(isEven(n)){
+            System.out.println("It is even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
